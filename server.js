f8f12e2a5e41f0ef5a72e147406d8372736d77ff
Alejandro Castillo <61676026+alejandro-castillo-delgado@users.noreply.github.com>
2023-09-02 12:56:19 -0400

    add files

---
+++ server.js
@@ -0,0 +1,28 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        console.log('Received:', message);
+        if (message.toString() === 'Hola') {   ws.send('Si funciona!');  }
+    });
+
+    ws.send('Hello from Node.js WebSocket server');
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
+
+
+// const WebSocket = require('ws');
+//   wss = new WebSocket.Server({port:8080});
+
+// wss.on('connection', function (socket) { 
+//   socket.on('message', function (msg) {
+//     console.log('Recibido: ', msg, '\n', 'Desde la IP: ', socket.upgradeReq.connection.remoteAddress);
+//     if (msg === 'Hola') {   socket.send('Si funciona!');  }
+//   });
+//    socket.on('close', function (code, desc) {
+//     console.log('Desconectado: ' + code + ' - ' + desc);
+//   });
+// });
