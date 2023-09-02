28936b3214038ebda24a2a98e9199668b3f4c978
Alejandro <alejandro.castillo.delgado.97@gmail.com>
2023-09-02 13:19:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server();
 
 wss.on('connection', ws => {
     ws.on('message', message => {
